251ebcd5365f546fd22d5a5a2dabd758fe5b4c24
Mr.doob <info@mrdoob.com>
2014-02-06 01:30:37 +0100

    Simplified audio loading code. Thanks @spite!

--- Audio.js
+++ Audio.js
@@ -1,4 +1,4 @@
-var Audio = function ( url ) {
+var Audio = function ( element ) {
 
 	var AudioContext = window.AudioContext || window.webkitAudioContext;
 	var context = new AudioContext();
@@ -11,93 +11,31 @@ var Audio = function ( url ) {
 	gain.gain.value = 10;
 	gain.connect( analyser );
 
-	var onLoadCallback = function () {};
-
-	var request = new XMLHttpRequest();
-	request.open( 'GET', url, true );
-	request.responseType = 'arraybuffer';
-	request.onload = function () {
-
-		context.decodeAudioData( request.response, function ( buffer ) {
-
-			var source = context.createBufferSource();
-			source.buffer = buffer;
-			source.connect( gain );
-
-			init();
-
-			source.start( 0 );
-
-			onLoadCallback();
-
-		} );
-
-	};
-	request.send();
+	var source = context.createMediaElementSource( element );
+	source.connect( gain );
 
 	//
 
-	var frequencyData = [];
+	var frequencyData = new Uint8Array( analyser.frequencyBinCount );
 
-	var frequencyCanvas = document.createElement( 'canvas' );
-	frequencyCanvas.style.position = 'absolute';
-	frequencyCanvas.style.top = '0px';
-	frequencyCanvas.style.left = '0px';
+	return {
 
-	var frequencyContext;
+		mark: 0,
+		getFrequency: function () {
 
-	var init = function () {
+			return frequencyData;
 
-		frequencyData = new Uint8Array( analyser.frequencyBinCount );
+		},
+		getCurrentTime: function () {
 
-		frequencyCanvas.width = frequencyData.length * 2;
-		frequencyCanvas.height = 256;
+			return context.currentTime;
 
-		frequencyContext = frequencyCanvas.getContext( '2d' );
+		},
+		update: function () {
 
-	};
-
-	this.mark = 0;
-
-	this.onLoad = function ( callback ) {
-
-		onLoadCallback = callback;
-
-	};
-
-	this.getCanvas = function () {
-
-		return frequencyCanvas;
-
-	};
-
-	this.getFrequency = function () {
-
-		return frequencyData;
-
-	};
-
-	this.getCurrentTime = function () {
-
-		return context.currentTime;
-
-	};
-
-	this.update = function () {
-
-		// analyser.smoothingTimeConstant = 0.1;
-		analyser.getByteFrequencyData( frequencyData );
-
-		/*
-		frequencyContext.clearRect( 0, 0, frequencyCanvas.width, frequencyCanvas.height );
-
-		for ( var i = 0, l = frequencyData.length; i < l; i ++ ) {
-
-			frequencyContext.fillStyle = i === this.mark ? 'white' : 'red';
-			frequencyContext.fillRect( i * 2, 0, 2, frequencyData[ i ] );
+			analyser.getByteFrequencyData( frequencyData );
 
 		}
-		*/
 
 	};
 
